71c6887668677c95b78d38ee176e0ef355d4a28c
sickwearxyz-us <sickwear.xyz@gmail.com>
2026-05-07 12:29:57 +0530

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+import type { MetadataRoute } from "next"
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://www.sickwear.xyz",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: "https://www.sickwear.xyz/overview",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: "https://www.sickwear.xyz/brand",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+  ]
+}
